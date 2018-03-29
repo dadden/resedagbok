cd120eaca202c891cfe57da61efbcd8a69b4994f
David Mohlén Bertlin <david.bertlin@learnet.se>
2018-03-29 14:22:45 +0200

    CSS regler, HTML scrapkod

--- index.php
+++ index.php
@@ -1,11 +1,58 @@
 <?php
 /**
-* BESKRIVNING AV PROGRAMMET
+* Resedagboken - ett tufft skolprojekt i PHP
 *
 * PHP version 5
-* @category   Kategori
+* @category   Resejournal/dagbok/blogg/bildgalleri
 * @author     David Mohlén <davidmohlen@gmail.com>
 * @license    PHP CC
 * @link
 */
 ?>
+    <!DOCTYPE html>
+    <html lang="sv">
+
+    <head>
+        <meta charset="utf-8">
+        <title>Startsida | Resedagboken</title>
+        <link rel="stylesheet" href="assets/style.css">
+    </head>
+
+    <body>
+        <div class="container">
+
+            <header>
+                <h1>Resedagboken</h1>
+                <nav>
+                    <ul>
+                        <li><a href="#" class="active">Logga in</a></li>
+                        <li><a href="#">Skapa konto</a></li>
+                        <li><a href="#">Andras resor</a></li>
+                    </ul>
+                </nav>
+
+                <form action="#" method="post">
+                    <input type="text" placeholder="Sök..." name="sok">
+                </form>
+            </header>
+
+            <main>
+
+                <img src="img/exempelbild.jpg" alt="Resebild">
+                <h2>På besök i ett ett tempel</h2>
+                <p>Lorem ipsum dolor amet semiotics knausgaard freegan, microdosing quinoa poutine fam enamel pin iceland lumbersexual pitchfork. Tumeric meh semiotics umami pinterest kinfolk asymmetrical cornhole biodiesel seitan raclette hammock shoreditch williamsburg pitchfork. Tacos put a bird on it pok pok intelligentsia humblebrag blue bottle edison bulb meh tousled enamel pin coloring book af craft beer synth. Sustainable freegan affogato live-edge pour-over, vexillologist DIY shaman raw denim pop-up snackwave craft beer. Green juice ugh four dollar toast air plant street art. Sartorial cray VHS keffiyeh salvia quinoa. Snackwave whatever organic quinoa.</p>
+            </main>
+
+            <footer>
+
+                <h4><a href="#">Info</a></h4>
+                <p>...</p>
+                <h4><a href="#">Kontakt</a></h4>
+                <p>...</p>
+
+            </footer>
+
+        </div>
+    </body>
+
+    </html>
